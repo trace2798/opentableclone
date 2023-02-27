5556720bf0f7a1d45ee6071605a03c10ca2f7eb1
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-02-28 00:56:55 +0530

    Added head file for different pages

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,22 +1,17 @@
 import React from "react";
-import Link from "next/link";
-import NavBar from "../../../components/NavBar";
 import Header from "../components/Header";
 import RestaurantNavBar from "../components/RestaurantNavBar";
 import Menu from "../components/Menu";
 
-const RestaurantMenu = () => {
+export default function RestaurantMenu()  {
   return (
     <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
         <div className="bg-white w-[100%] rounded p-3 shadow">
           <RestaurantNavBar />
           <Menu />
         </div>
-      </div>
     </>
   );
 };
 
-export default RestaurantMenu;
+
