99923893e807c779ee00fea6a939083fe2e1b49d
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-02-28 16:24:23 +0530

    added dynamic func  to render res properties

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 
-const Header = () => {
+export default function Header({ name }: { name: string }) {
+  const renderTitle = () => {
+    //declares a variable named "nameArray" by calling the "split" method on a variable named "name".
+    const nameArray = name.split("-");
+    //modifies the last element of the "nameArray" by adding parentheses around it
+    nameArray[nameArray.length - 1] = `(${nameArray[nameArray.length - 1]})`;
+    //returns a new string by joining all the elements of the "nameArray" array together with a space separator using the "join" method.
+    return nameArray.join(" ")
+  };
+
   return (
     <div className="h-96 overflow-hidden">
       <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
-        <h1 className="text-7xl text-white captitalize text-shadow text-center">
-          Milestones Grill (Toronto)
+        <h1 className="text-7xl text-white capitalize text-shadow text-center">
+          {renderTitle()}
         </h1>
       </div>
     </div>
   );
-};
-
-export default Header;
+}
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -6,15 +6,44 @@ import Description from "./components/Description";
 import Images from "./components/Images";
 import Reviews from "./components/Reviews";
 import ReservationCard from "./components/ReservationCard";
-const RestaurantDetails = () => {
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+  return restaurant;
+};
+
+export default async function RestaurantDetails({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
-        <RestaurantNavBar />
-        <Title />
+        <RestaurantNavBar slug={restaurant.slug} />
+        <Title name={restaurant.name}/>
         <Rating />
-        <Description />
-        <Images />
+        <Description description={restaurant.description} />
+        <Images images={restaurant.images}/>
         <Reviews />
       </div>
       <div className="w-[27%] relative text-reg">
@@ -22,6 +51,4 @@ const RestaurantDetails = () => {
       </div>
     </>
   );
-};
-
-export default RestaurantDetails;
+}
