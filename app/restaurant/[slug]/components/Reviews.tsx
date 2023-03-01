5521a67744da599c15caa2c49e185f1b2565d9db
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-03-01 20:05:49 +0530

    functionality to dynamically fetch reviews added

--- app/restaurant/[slug]/components/Reviews.tsx
+++ app/restaurant/[slug]/components/Reviews.tsx
@@ -1,6 +1,8 @@
+import { Review } from "@prisma/client";
 import React from "react";
+import ReviewCard from "./ReviewCard";
 
-export default function Reviews({ reviews }: { reviews: string[] }) {
+export default function Reviews({ reviews }: { reviews: Review[] }) {
   return (
     <div>
       <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
@@ -9,30 +11,8 @@ export default function Reviews({ reviews }: { reviews: string[] }) {
       <div>
         {/* REVIEW CARD */}
         {reviews.map((review) => (
-          
-   
-        <div className="border-b pb-7 mb-7">
-          <div className="flex">
-            <div className="w-1/6 flex flex-col items-center">
-              <div className="rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center">
-                <h2 className="text-white text-2xl"> {review.first_name[0]}
-              {review.last_name[0]}</h2>
-              </div>
-              <p className="text-center">{review.first_name}{" "}{review.last_name}</p>
-            </div>
-            <div className="ml-10 w-5/6">
-              <div className="flex items-center">
-                <div className="flex mr-5">*****</div>
-              </div>
-              <div className="mt-5">
-                <p className="text-lg font-light">
-                  {review.text}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-             ))}
+          <ReviewCard review={review} key={review.id}/>
+        ))}
         {/* REVIEW CARD */}
       </div>
     </div>
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -41,7 +41,7 @@ export default async function RestaurantDetails({
       <div className="bg-white w-[70%] rounded p-3 shadow">
         <RestaurantNavBar slug={restaurant.slug} />
         <Title name={restaurant.name}/>
-        <Rating />
+        <Rating reviews={restaurant.reviews}  />
         <Description description={restaurant.description} />
         <Images images={restaurant.images}/>
         <Reviews reviews={restaurant.reviews} />
