5efb24f8798c4a2b25095c46c3b9be47c20f601a
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-03-03 19:41:48 +0530

    signup from front-end functionality added

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -6,13 +6,17 @@ const useAuth = () => {
   const { data, error, loading, setAuthState } = useContext(
     AuthenticationContext
   );
-  const signin = async ({
-    email,
-    password,
-  }: {
-    email: string;
-    password: string;
-  }, handleClose: () => void) => {
+
+  const signin = async (
+    {
+      email,
+      password,
+    }: {
+      email: string;
+      password: string;
+    },
+    handleClose: () => void
+  ) => {
     setAuthState({ data: null, error: null, loading: true });
     try {
       const response = await axios.post(
@@ -36,7 +40,52 @@ const useAuth = () => {
       });
     }
   };
-  const signup = async () => {};
+
+  const signup = async (
+    {
+      firstName,
+      lastName,
+      phone,
+      city,
+      email,
+      password,
+    }: {
+      firstName: string;
+      lastName: string;
+      phone: string;
+      city: string;
+      email: string;
+      password: string;
+    },
+    handleClose: () => void
+  ) => {
+    setAuthState({ data: null, error: null, loading: true });
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/signup",
+        {
+          firstName,
+          lastName,
+          phone,
+          city,
+          email,
+          password,
+        }
+      );
+      setAuthState({
+        data: response.data,
+        error: null,
+        loading: false,
+      });
+      handleClose();
+    } catch (error: any) {
+      setAuthState({
+        data: null,
+        error: error.response.data.errorMessage,
+        loading: false,
+      });
+    }
+  };
 
   return {
     signin,
--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -3,6 +3,7 @@ import validator from "validator";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import * as jose from "jose";
+import { setCookie } from "cookies-next";
 
 const prisma = new PrismaClient();
 
@@ -92,8 +93,14 @@ export default async function handler(
       .setExpirationTime("24h")
       .sign(secret);
 
+    setCookie("jwt", token, { req, res, maxAge: 60 * 6 * 24 });
+
     res.status(200).json({
-      hello: user,
+      firstName: user.first_name,
+      lastName: user.last_name,
+      email: user.email,
+      phone: user.phone,
+      city: user.city,
     });
   }
   return res.status(404).json("Unknown endpoint");
