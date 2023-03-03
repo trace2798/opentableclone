521bf151b2b408fcdfeecaa20ad05b011e02ab89
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-03-03 23:36:33 +0530

    added functionality to dynamically render time

--- app/restaurant/[slug]/components/ReservationCard.tsx
+++ app/restaurant/[slug]/components/ReservationCard.tsx
@@ -1,16 +1,48 @@
 "use client";
 import { partySize, times } from "../../../../data";
-import DatePicker from 'react-datepicker'
+import DatePicker from "react-datepicker";
 import { useState } from "react";
 
-export default function ReservationCard() {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date())
-const handleChangeDate = (date: Date | null) => {
-  if(date){
-    return setSelectedDate(date)
-  }
-  return setSelectedDate(null);
-}
+export default function ReservationCard({
+  openTime,
+  closeTime,
+}: {
+  openTime: string;
+  closeTime: string;
+}) {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const handleChangeDate = (date: Date | null) => {
+    if (date) {
+      return setSelectedDate(date);
+    }
+    return setSelectedDate(null);
+  };
+
+  //this function filters a given array of time objects to return only the ones that fall within a specified open window of a restaurant.
+  const filterTimeByRestaurantOpenWindow = () => {
+    //window is the time from when they are open till they close.
+    //initializes  to an empty array.
+    const timesWithinWindow: typeof times = [];
+    // variable called isWithinWindow is defined and initialized to false.
+    let isWithinWindow = false;
+    //forEach method is called on the times array.
+    times.forEach((time) => {
+      //If the time object's time property matches the openTime value, the isWithinWindow variable is set to true.
+      if (time.time === openTime) {
+        isWithinWindow = true;
+      }
+      // If isWithinWindow is true, the current time object is pushed onto the timesWithinWindow array.
+      if (isWithinWindow) {
+        timesWithinWindow.push(time);
+      }
+      //If the time object's time property matches the closeTime value, the isWithinWindow variable is set back to false.
+      if (time.time === closeTime) {
+        isWithinWindow = false;
+      }
+    });
+    //timesWithinWindow array is returned from the function.
+    return timesWithinWindow;
+  };
 
   return (
     <div className="fixed w-[15%] bg-white rounded p-3 shadow">
@@ -28,16 +60,22 @@ const handleChangeDate = (date: Date | null) => {
       <div className="flex justify-between">
         <div className="flex flex-col w-[48%]">
           <label htmlFor="">Date</label>
-          <DatePicker selected={selectedDate} onChange={handleChangeDate} className="py-3 border-b font-light text-reg w-28" dateFormat="MMMM d" wrapperClassName="w-[48%]"/>
+          <DatePicker
+            selected={selectedDate}
+            onChange={handleChangeDate}
+            className="py-3 border-b font-light text-reg w-24"
+            dateFormat="MMMM d"
+            wrapperClassName="w-[48%]"
+          />
         </div>
+        
         <div className="flex flex-col w-[48%]">
           <label htmlFor="">Time</label>
           <select name="" id="" className="py-3 border-b font-light">
-          {times.map((time) => (
-            <option value={time.time}>{time.displayTime}</option>
-          ))}
-            <option value="">7:30 AM</option>
-            <option value="">9:30 AM</option>
+           {/* Now it will dynamically show the different time options based on the window of the restaurant.  */}
+            {filterTimeByRestaurantOpenWindow().map((time) => (
+              <option value={time.time}>{time.displayTime}</option>
+            ))}
           </select>
         </div>
       </div>
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -5,10 +5,21 @@ import Description from "./components/Description";
 import Images from "./components/Images";
 import Reviews from "./components/Reviews";
 import ReservationCard from "./components/ReservationCard";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Review } from "@prisma/client";
 import { notFound } from "next/navigation";
 
 const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  open_time: string;
+  close_time: string;
+  slug: string;
+  reviews: Review[];
+}
 
 const fetchRestaurantBySlug = async (slug: string) => {
   const restaurant = await prisma.restaurant.findUnique({
@@ -22,6 +33,8 @@ const fetchRestaurantBySlug = async (slug: string) => {
       description: true,
       slug: true,
       reviews: true,
+      open_time: true,
+      close_time: true,
     },
   });
 
@@ -42,14 +55,17 @@ export default async function RestaurantDetails({
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
         <RestaurantNavBar slug={restaurant.slug} />
-        <Title name={restaurant.name}/>
-        <Rating reviews={restaurant.reviews}  />
+        <Title name={restaurant.name} />
+        <Rating reviews={restaurant.reviews} />
         <Description description={restaurant.description} />
-        <Images images={restaurant.images}/>
+        <Images images={restaurant.images} />
         <Reviews reviews={restaurant.reviews} />
       </div>
       <div className="w-[27%] relative text-reg">
-        <ReservationCard />
+        <ReservationCard
+          openTime={restaurant.open_time}
+          closeTime={restaurant.close_time}
+        />
       </div>
     </>
   );
