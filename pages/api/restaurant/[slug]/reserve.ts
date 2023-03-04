44f8551def84fdb15e32dd11b4eaeca265f5ba77
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-03-04 23:18:37 +0530

    validate restaurant and working window

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -2,7 +2,12 @@ import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { times } from "../../../../data";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { slug, day, time, partySize } = req.query as {
     slug: string;
     day: string;
@@ -10,13 +15,33 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     partySize: string;
   };
 
-  return res.json({
-    slug,
-    day,
-    time,
-    partySize,
+  //Step1: validate the restaurant exists and that the day is within the opening hours.
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
   });
+
+  if (!restaurant) {
+    return res.status(400).json({
+      errorMessage: "Restaurant does not exist in database",
+    });
+  }
+
+  if (
+    new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
+    new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
+  ) {
+    return res.status(400).json({
+      errorMessage: "Restaurant is not open at that time",
+    });
+  }
+  // return res.json({
+  //   slug,
+  //   day,
+  //   time,
+  //   partySize,
+  // });
 }
 
 // http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-05-27&time=15:00:00.000Z&partySize=8
-
